9e3b9a8a0d07c3a6c4c936ba33b1674aec8ca395
James Wol <james.wol@petalmail.com>
2026-07-10 01:43:16 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,6 @@ from pathlib import Path
 from datetime import datetime, timedelta
 import numpy as np
 import pandas as pd
-import plotly.graph_objects as go
 import plotly.express as px
 import requests
 from plotly.subplots import make_subplots
